aafdcc61e62f4244e9647dd3a70379e250bcf88b
Kenvix <kenvixzure@live.com>
2019-03-31 17:42:26 +0800

    fix permission issues

--- ci-cron.sh
+++ ci-cron.sh
@@ -4,6 +4,7 @@ git config --global user.email "$CERT_GIT_EMAIL"
 git config --global user.name "$CERT_GIT_USER"
 git clone --recursive --branch $CERT_GIT_BRANCH --depth=1 "$CERT_GIT_SCHEME$CERT_GIT_USER:$CERT_GIT_PASSWORD@$CERT_GIT_URI" ./data
 
+chmod -R 777 ./data
 ls -al ./data
 
 ./cron.sh
