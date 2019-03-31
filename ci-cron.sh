f6b772c04ddf89f528333629e81bbb55a973ae28
Kenvix <kenvixzure@live.com>
2019-03-31 17:40:25 +0800

    abort when cron.sh failed

--- ci-cron.sh
+++ ci-cron.sh
@@ -2,9 +2,16 @@ echo "[AutoCert] Cron for CI"
 
 git config --global user.email "$CERT_GIT_EMAIL"
 git config --global user.name "$CERT_GIT_USER"
-git clone --progress --recursive --branch $CERT_GIT_BRANCH --depth=1 "$CERT_GIT_SCHEME$CERT_GIT_USER:$CERT_GIT_PASSWORD@$CERT_GIT_URI" ./data
+git clone --recursive --branch $CERT_GIT_BRANCH --depth=1 "$CERT_GIT_SCHEME$CERT_GIT_USER:$CERT_GIT_PASSWORD@$CERT_GIT_URI" ./data
+
+ls -al ./data
 
 ./cron.sh
+
+if [ $? -ne 0 ]
+then
+    exit $?
+fi
 
 git add .
 git commit -m "$CERT_GIT_COMMIT_MESSAGE" -v -a
