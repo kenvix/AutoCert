9f0240653620ee96026835992e892b9055b5b97f
Kenvix Zure <kenvixzure@live.com>
2020-05-09 19:32:20 +0800

    Update client

--- client.sh
+++ client.sh
@@ -2,11 +2,20 @@
 
 source ./init.sh
 
-if [ ! -f "./data/.git/HEAD" ]
-then
-    echo "[AutoCert] Cert git not cloned"
-    git clone --recursive --branch $CERT_GIT_BRANCH --depth=1 "$CERT_GIT_SCHEME$CERT_GIT_USER:$CERT_GIT_PASSWORD@$CERT_GIT_URI" ./data
+if [ ! -f "./data/.git/HEAD" ]; then
+    echo "[AutoCert] Cloning Cert git ..."
+
+    if [ -f "./deployment.key" ]; then
+        ssh-agent $(ssh-add ./deployment.key; git clone --recursive --branch $CERT_GIT_BRANCH --depth=1 "$CERT_GIT_URI" ./data; ssh-add -d ./deployment.key )
+    else
+        git clone --recursive --branch $CERT_GIT_BRANCH --depth=1 "$CERT_GIT_URI" ./data
+    fi
 else
     cd data
     git.exe pull -v --no-rebase "origin" $CERT_GIT_BRANCH   
+fi
+
+if [ -f "./after-update-cert.sh" ]; then
+    echo "[AutoCert] Running cert update callback after-update-cert.sh"
+    ./after-update-cert.sh
 fi
--- init.sh
+++ init.sh
@@ -1,14 +1,14 @@
 if [ -z $AUTOCERT_INITED ]
 then
-    if [ ! -x "./data/config.sh" ]; then
-        echo "Error: ./data/config.sh not found or not executable"
-        echo "Please modify config.sh.example and copy it to ./data/config.sh"
+    if [ ! -x "./config.sh" ]; then
+        echo "Error: ./config.sh not found or not executable"
+        echo "Please modify config.sh.example and copy it to ./config.sh"
         exit 170
     fi
     export AUTOCERT_INITED=1
 fi
 
-source ./data/config.sh
+source ./config.sh
 
 RunACME(){
     if [ "$ACME_MERGED_PARAMS" ]
