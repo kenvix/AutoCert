186635e56c89115d75593ca239c560cdc517dab4
Kenvix Zure <kenvixzure@live.com>
2020-06-07 20:49:10 +0800

    Add pfx generation

--- cron.sh
+++ cron.sh
@@ -8,5 +8,21 @@ else
     export ACME_MERGED_PARAMS="--cron "
 
     RunACME
-    exit $?    
+    if [ $? -ne 0 ]; then
+        echo "[AutoCert] acme.sh failed with code $?"
+        exit $? 
+    fi
+
+    echo "[AutoCert] acme.sh shell finished"
+
+    pushd "./data/acme/${ACME_DOMAINS[0]}"
+    currentTime=$(date +%s)
+    modifyTime=$(stat -c %Y "${ACME_DOMAINS[0]}.key")
+    timeDiff=$(($currentTime-$modifyTime))
+    
+    if [ $timeDiff -lt 60 ] || [ ! -f "${ACME_DOMAINS[0]}.pfx" ]; then 
+        echo "[AutoCert] Generating pfx for ${ACME_DOMAINS[0]}"
+        openssl pkcs12 -export -out "${ACME_DOMAINS[0]}.pfx" -inkey "${ACME_DOMAINS[0]}.key" -in fullchain.cer -passout pass:
+    fi
+    popd
 fi
